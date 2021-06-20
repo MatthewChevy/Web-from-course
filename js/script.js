7a903a99074230c454df066df9f9f4a9b62382cd
matthew.chevy <matej.sevcik11@gmail.com>
2021-06-20 14:25:21 +0200

    push project on github

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+(function toggleMenu(){
+
+if ( window.innerWidth >= 748 ){
+
+    list[0].style.display = "block"
+} else {
+
+    let list = document.querySelectorAll('.ul-nav-bar'),
+        menu = document.querySelectorAll('.menu-nav-bar');
+
+        menu[1].addEventListener('click', function() {  
+        
+            if (  list[0].style.display === "block" ) {
+                list[0].style.display = "none";
+            } else {
+                list[0].style.display = "block"
+            }
+    });
+
+};
+
+}() );
+
+	
+
+
